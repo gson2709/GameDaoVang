c950a7eb3f3f09689f4b3b3ab182cabd0fcd55f2
Nguyen Son <truongson9a2nd@gmail.com>
2024-11-30 09:19:48 +0700

    Fix CharacterDao

--- src/main/java/com/example/thuctapxuong/dao/CharacterDao.java
+++ src/main/java/com/example/thuctapxuong/dao/CharacterDao.java
@@ -1,5 +1,6 @@
 package com.example.thuctapxuong.dao;
 
+import com.example.thuctapxuong.entity.Character;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CharacterDao extends JpaRepository<Character, Integer> {
